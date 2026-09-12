27beeab96fc6174caca46de494621b288186e7bb
Daniel <danielfernando118158@gmail.com>
2026-09-12 11:41:10 +0800

    fixing still home

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("static"));
+app.use(express.static(path.join(__dirname, "/static")));
 app.use("/signup", signup);
 app.use("/login", login);
 
